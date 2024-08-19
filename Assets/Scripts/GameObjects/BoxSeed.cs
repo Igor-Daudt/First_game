cd015b2d07475ffbb65676a05976b964478d96ba
unknown <igordaudt1@gmail.com>
2024-08-19 18:03:11 -0300

    Hover_animation_BoxSeed

--- Assets/Scripts/GameObjects/BoxSeed.cs
+++ Assets/Scripts/GameObjects/BoxSeed.cs
@@ -5,8 +5,22 @@ using UnityEngine;
 public class BoxSeed : MonoBehaviour
 {
     [SerializeField] private Item seed;
+    [SerializeField] Texture2D cursorNormal;
+    [SerializeField] Texture2D cursorHovering;
     
     public void Interact(){
         InventoryController.instance.AddItem(seed);
+    }
+
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorHovering, new Vector2(0,0), CursorMode.Auto);
+        Debug.Log("Mouse is over GameObject.");
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(cursorNormal, new Vector2(0,0), CursorMode.Auto);
+        Debug.Log("Mouse is no longer on GameObject.");
     }
 }
